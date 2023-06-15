a470e6b7f8908de940c0df2968d7e1256bcb1524
Yuri WIllians dos Santos <42418401+yuri-willians@users.noreply.github.com>
2023-06-15 19:05:04 -0300

    feat: adiciona insert para `CAR_DRIVER`

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,6 @@ INSERT INTO driver VALUES (1, '61387837010', '1999-04-01')
 
 -- CUSTOMER
 INSERT INTO customer VALUES (1, 'Paulo Marcos de Andrade', 1)
+
+-- CAR_DRIVER
+INSERT INTO car_driver VALUES (1, 1, 1, true)
