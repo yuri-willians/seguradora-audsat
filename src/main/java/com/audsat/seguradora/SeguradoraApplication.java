b020e369b176f90645ebb46b1b8639d2ce5e35fb
Yuri WIllians dos Santos <42418401+yuri-willians@users.noreply.github.com>
2023-06-14 23:47:04 -0300

    chore: adiciona scripts de criação de `schema` e `insert` para H2Database

--- src/main/java/com/audsat/seguradora/SeguradoraApplication.java
+++ src/main/java/com/audsat/seguradora/SeguradoraApplication.java
@@ -2,12 +2,13 @@ package com.audsat.seguradora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SeguradoraApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SeguradoraApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(SeguradoraApplication.class, args);
+    }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+-- CAR
+INSERT INTO car VALUES (1, 'UNO', 'Fiat', '2010', 10000)
+
+-- DRIVER
+INSERT INTO driver VALUES (1, '61387837010', '1999-04-01')
+
+-- CUSTOMER
+INSERT INTO customer VALUES (1, 'Paulo Marcos de Andrade', 1)
