84932ed8b1054eeeaaaa7fa26e5b2861a14f66be
Yuri WIllians dos Santos <42418401+yuri-willians@users.noreply.github.com>
2023-06-16 13:54:13 -0300

    test: adiciona novas itens na database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,17 @@
 -- CAR
-INSERT INTO car VALUES (1, 'UNO', 'Fiat', '2010', 10000)
+INSERT INTO car
+VALUES (1, 'UNO', 'Fiat', '2010', 10000),
+       (2, 'Toro', 'Fiat', '2021', 50000);
 
 -- DRIVER
-INSERT INTO driver VALUES (1, '61387837010', '1999-04-01')
+INSERT INTO driver
+VALUES (1, '61387837010', '1999-04-01');
 
 -- CUSTOMER
-INSERT INTO customer VALUES (1, 'Paulo Marcos de Andrade', 1)
+INSERT INTO customer
+VALUES (1, 'Paulo Marcos de Andrade', 1);
 
 -- CAR_DRIVER
-INSERT INTO car_driver VALUES (1, 1, 1, true)
+INSERT INTO car_driver
+VALUES (1, 1, 1, true),
+       (2, 1, 2, true);
